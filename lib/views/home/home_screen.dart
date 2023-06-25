5e0c0833d87669bc0c726b87a4e7092c72305372
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-25 08:41:56 +0600

    upload file

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:erase/views/home/widgets/bottom_navbar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +14,24 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool loaded = false;
+  bool imagePicked = false;
+  Uint8List image = Uint8List(0);
+  String imagePath = "";
+
+  // pick image from gallery
+  Future<void> pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image =
+        await picker.pickImage(source: ImageSource.gallery, imageQuality: 100);
+    if (image != null) {
+      setState(() {
+        imagePath = image.path;
+        imagePicked = true;
+      });
+    }
+  }
+
+  // pick image end
 
   @override
   Widget build(BuildContext context) {
@@ -20,12 +42,14 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: const Icon(Icons.sort_rounded),
         title: const Text("Erase: Remove Background"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text("Remove Background"),
-        ),
-      ),
+      body: imagePicked
+          ? Image.file(File(imagePath))
+          : Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Remove Background"),
+              ),
+            ),
       bottomNavigationBar: BottomNav(
         onPress: loaded ? () {} : null,
       ),
