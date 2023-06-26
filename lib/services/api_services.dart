8527173e4e3cd6502010c5c0c9385cdd4ab03d9b
MD Rabbi Hossen <ariyanrabby94@gmail.com>
2023-06-26 14:36:55 +0600

    api service update

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -2,10 +2,21 @@ import 'dart:io';
 
 import 'package:erase/utils/const.dart';
 import 'package:http/http.dart' as http;
-class ApiServices{
+
+class ApiServices {
   // remove bg
   static removeBackground(String imagePath) async {
-    http.MultipartRequest requestUrl=http.MultipartRequest("POST",Api.baseUrl);
-    requestUrl.headers.addAll({"X-Api-Key":Api.apiKey});
+    http.MultipartRequest requestUrl =
+        http.MultipartRequest("POST", Api.baseUrl);
+    requestUrl.headers.addAll({"X-Api-Key": Api.apiKey});
+    requestUrl.files
+        .add(await http.MultipartFile.fromPath("image_file", imagePath));
+    http.StreamedResponse response = await requestUrl.send();
+    if (response.statusCode == 200) {
+      http.Response imageResponse = await http.Response.fromStream(response);
+      return imageResponse.bodyBytes;
+    } else {
+      return null;
+    }
   }
 }
